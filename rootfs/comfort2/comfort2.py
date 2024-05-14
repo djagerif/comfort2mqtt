7f8a71695d0fc0c1c62f905132861f983914524a
Ingo de Jager <djagerif@users.noreply.github.com>
2024-05-14 21:45:27 +0200

    test

--- rootfs/comfort2/comfort2.py
+++ rootfs/comfort2/comfort2.py
@@ -1214,7 +1214,10 @@ class Comfort2(mqtt.Client):
                                 ipMsg = ComfortIPInputActivationReport(line[1:])
                                 publish_result = self.publish(ALARMINPUTTOPIC % ipMsg.input, ipMsg.state,qos=0,retain=True)
                                 logger.debug("result: %s", publish_result)
-                                publish_result.wait_for_publish(1)
+                                if publish_result.is_published() == False:
+                                    print('Message is not yet published.')
+                                    # This call will block until the message is published.
+                                    publish_result.wait_for_publish(1)
                             elif line[1:3] == "CT":
                                 ipMsgCT = ComfortCTCounterActivationReport(line[1:])
                                 publish_result = self.publish(ALARMCOUNTERINPUTRANGE % ipMsgCT.counter, ipMsgCT.value,qos=0,retain=True)     # Value Information
