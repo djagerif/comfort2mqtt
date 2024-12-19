6e01f52a7f18f48d37eda1bb652b4e9ffb156030
Ingo de Jager <djagerif@users.noreply.github.com>
2024-12-19 08:00:45 +0200

    test device_properties

--- rootfs/comfort2/comfort2.py
+++ rootfs/comfort2/comfort2.py
@@ -2603,6 +2603,8 @@ class Comfort2(mqtt.Client):
                                     device_properties['ChargerStatus'] = "N/A"
                                     device_properties['BatteryStatus'] = "N/A"
 
+                                logging.debug("device_properties: %s", device_properties)
+                                
                                 self.UpdateDeviceInfo(True)     # Update Device properties.
 
                             elif line[1:3] == "EL":       # Determine HW model number CM9000/9001 if available and number of Slave confirmation.
