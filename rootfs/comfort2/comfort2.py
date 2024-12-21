844cf109bf1c264951f7cfe0a6214893e884d52e
Ingo de Jager <djagerif@users.noreply.github.com>
2024-12-21 07:26:15 +0200

    Add check for SCS/RIO Inputs and Outputs. Don't poll if set to 0.

--- rootfs/comfort2/comfort2.py
+++ rootfs/comfort2/comfort2.py
@@ -1415,21 +1415,26 @@ class Comfort2(mqtt.Client):
             SAVEDTIME = datetime.now()
             time.sleep(0.1)
 
-            logging.debug("Config z value: %s", str(COMFORT_RIO_INPUTS))
+            #logging.debug("Config z value: %s", str(COMFORT_RIO_INPUTS))
             #get all SCS/RIO input states
-            self.comfortsock.sendall("\x03z?\r".encode())       # Comfort SCS/RIO Inputs
-            SAVEDTIME = datetime.now()
-            time.sleep(0.1)
-            #get all output states
-            self.comfortsock.sendall("\x03Y?\r".encode())
-            SAVEDTIME = datetime.now()
-            time.sleep(0.1)
+            if COMFORT_RIO_INPUTS > 0:
+                self.comfortsock.sendall("\x03z?\r".encode())       # Comfort SCS/RIO Inputs
+                SAVEDTIME = datetime.now()
+                time.sleep(0.1)
 
-            logging.debug("Config y value: %s", str(COMFORT_RIO_OUTPUTS))
+            #get all output states
+            if ALARMNUMBEROFOUTPUTS > 0:
+                self.comfortsock.sendall("\x03Y?\r".encode())
+                SAVEDTIME = datetime.now()
+                time.sleep(0.1)
+
+            #logging.debug("Config y value: %s", str(COMFORT_RIO_OUTPUTS))
             #get all RIO output states
-            self.comfortsock.sendall("\x03y?\r".encode())       # Request/Report all SCS/RIO Outputs
-            SAVEDTIME = datetime.now()
-            time.sleep(0.1)
+            if COMFORT_RIO_OUTPUTS > 0:
+                self.comfortsock.sendall("\x03y?\r".encode())       # Request/Report all SCS/RIO Outputs
+                SAVEDTIME = datetime.now()
+                time.sleep(0.1)
+
             #get all flag states
             self.comfortsock.sendall("\x03f?00\r".encode())
             SAVEDTIME = datetime.now()
