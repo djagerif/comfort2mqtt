ba555220c37f846caa3d078c62e45d552337eb90
Ingo de Jager <djagerif@users.noreply.github.com>
2024-12-22 12:32:14 +0200

    test

--- rootfs/comfort2/comfort2.py
+++ rootfs/comfort2/comfort2.py
@@ -386,7 +386,7 @@ COMFORT_RESPONSES=int(option.alarm_responses)
 COMFORT_TIME=str(option.comfort_time)
 COMFORT_RIO_INPUTS=int(option.alarm_rio_inputs)
 COMFORT_RIO_OUTPUTS=int(option.alarm_rio_outputs)
-COMFORT_BATTERY_STATUS_ID=int(option.comfort_battery_update)
+COMFORT_BATTERY_STATUS_ID=option.comfort_battery_update
 
 logger.info("COMFORT_INPUTS: %s", str(COMFORT_INPUTS))
 logger.info("COMFORT_BATTERY_STATUS_ID: %s", str(COMFORT_BATTERY_STATUS_ID))
