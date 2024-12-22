63d06d7d373b341073d85153da05afae57fdc7d0
Ingo de Jager <djagerif@users.noreply.github.com>
2024-12-22 09:06:01 +0200

    test

--- rootfs/comfort2/comfort2.py
+++ rootfs/comfort2/comfort2.py
@@ -385,7 +385,7 @@ COMFORT_RESPONSES=int(option.alarm_responses)
 COMFORT_TIME=str(option.comfort_time)
 COMFORT_RIO_INPUTS=int(option.alarm_rio_inputs)
 COMFORT_RIO_OUTPUTS=int(option.alarm_rio_outputs)
-COMFORT_BATTERY_STATUS_ID=int(option.comfort_battery_status)
+COMFORT_BATTERY_STATUS_ID=int(option.comfort_battery_status_id)
 
 ALARMINPUTTOPIC = DOMAIN+"/input%d"                     #input1,input2,... input128 for every input. Physical Inputs (Default 8), Max 128
 if COMFORT_INPUTS < 8:
